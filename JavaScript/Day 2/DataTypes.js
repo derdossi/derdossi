ac56c3bb9e6f36be1eb8a8c35f240a7733c2ec0a
derdossi <manuelds@hotmail.de>
2022-04-01 15:21:47 +0200

    Primitve and non-primitve Data types

--- JavaScript/Day 2/DataTypes.js
+++ JavaScript/Day 2/DataTypes.js
@@ -1,3 +1,17 @@
+/* Primitive Data Types - CANNOT BE MODIFIED, are comparable by ==
+1. Numbers - Integers, float
+2. Strings - Any data under single quote, double quote or backtick quote
+3. Booleans - True or false
+4. Null - empty value
+5. Undefined
+NEW!!!
+Non-Primitive data types are non comparable
+1. Objects
+2. Functions+
+3. Arrays
+
+Example of non-modifiable Primitive Data type
+*/
 let word = "javascript"
 word[0] = 'Y'
 console.log(word)
