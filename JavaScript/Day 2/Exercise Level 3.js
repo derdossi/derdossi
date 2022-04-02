1200250b818183e54c36cd5128710c8ed03d5ecf
derdossi <manuelds@hotmail.de>
2022-04-02 19:33:33 +0200

    Day 2 finished?

---
+++ JavaScript/Day 2/Exercise Level 3.js
@@ -0,0 +1,6 @@
+let love = "Love is the best thing in this world. Some found their love and some are still looking for their love."
+let pattern = /Love/gi
+console.log(love.match(pattern))
+let because = "You cannot end a sentence with because because because is a conjunction"
+console.log(because.match(/because/gi))
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
