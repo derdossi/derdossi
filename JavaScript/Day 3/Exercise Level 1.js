897cc068814cee41556badbb621a7849500cfcbf
derdossi <manuelds@hotmail.de>
2022-04-08 21:15:00 +0200

    Day 3

---
+++ JavaScript/Day 3/Exercise Level 1.js
@@ -0,0 +1,62 @@
+// 1. Declare firstName, lastName, country, city, age, isMarried, 
+// year variable and assign value to it and use the typeof operator to check different data types.
+let firstName = "Boglárka"
+let lastName = "Ördög"
+let country = "Hungary"
+let city = "Kunszállás"
+let age = 20
+let isMarried = false
+let year = 2022
+
+console.log(typeof(firstName))
+console.log(typeof(lastName))
+console.log(typeof(country))
+console.log(typeof(city))
+console.log(typeof(age))
+console.log(typeof(isMarried))
+console.log(typeof(year))
+    // 2. Check if type of '10' is equal to 10
+let number = 10
+console.log(typeof(number) == 10)
+    // 3. Check if parseInt('9.8') is equal to 10
+console.log(parseInt(9.8) == 10)
+    /* 4. Boolean value is either true or false.
+    - Write three JavaScript statement which provide truthy value.
+    - Write three JavaScript statement which provide falsy value.
+    */
+let yes = true
+let yus = 6
+let truth = "Hello"
+let no = false
+let nyo = 0
+let boggi = undefined
+    /* 5. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+    i. true
+    ii. true
+    iii. false
+    iv. false
+    v. true
+    vi. true
+    vii. false
+    viii. false
+    ix. true // wrong
+    x. true
+    xi. false
+    */
+console.log(4 > 3) // true
+console.log(4 >= 3) // true
+console.log(4 < 3) // false
+console.log(4 <= 3) // false
+console.log(4 == 4) // true
+console.log(4 === 4) // true
+console.log(4 != 4) // false
+console.log(4 !== 4) // false
+console.log(4 != "4") // false
+console.log(4 == "4") // true
+console.log(4 === "4") // false
+let python = "python"
+let jargon = "jargon"
+console.log(python.length)
+console.log(jargon.length)
+console.log(jargon === python)
+    //
