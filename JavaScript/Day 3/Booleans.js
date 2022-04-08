315d8ac593c2b8ba4bf3ea5e33f4bbc5ed251920
derdossi <manuelds@hotmail.de>
2022-04-08 21:15:05 +0200

    Day 3

---
+++ JavaScript/Day 3/Booleans.js
@@ -0,0 +1,8 @@
+let isRaining = true
+isRaining
+    ?
+    console.log('You need a rain coat.') :
+    console.log('No need for a rain coat.')
+
+const now = new Date()
+console.log(now.getMonth())
