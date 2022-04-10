70215d0e2670b2a8935d6b8c6c914d161cdb5fc0
derdossi <manuelds@hotmail.de>
2022-04-10 23:41:06 +0200

    Day 3 contniued

---
+++ JavaScript/Day 3/Exercise Level 2.js
@@ -0,0 +1,27 @@
+/* 1 Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+let base = prompt("Enter Base")
+let height = prompt("Enter height")
+let area = 0.5 * base * height
+console.log(area)
+*/ // Commenting to have less prompts on live Server
+/* Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+let a = parseInt(prompt("Enter side a"))
+let b = parseInt(prompt("Enter side b"))
+let c = parseInt(prompt("Enter side c"))
+let perimeter = (a + b + c)
+console.log(`The perimeter of the triangle is ${perimeter}`)
+*/
+/*Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+let length = parseInt(prompt("Enter length"))
+let width = parseInt(prompt("Enter width"))
+let perimeter = 2 * (length + width)
+console.log(perimeter)
+*/
+/* Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+const pi = 3.14
+let radius = parseInt(prompt("Enter radius"))
+let area = pi * radius * radius
+let circumference = 2 * pi * radius
+console.log(area)
+console.log(circumference)
+*/
