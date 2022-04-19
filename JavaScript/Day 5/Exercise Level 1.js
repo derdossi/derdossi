4579f0bb1c7c2c6dc61ec0849893af084eb70126
derdossi <manuelds@hotmail.de>
2022-04-19 17:41:45 +0200

    Starting Day 5

---
+++ JavaScript/Day 5/Exercise Level 1.js
@@ -0,0 +1,13 @@
+/* console cleaning
+const empty = []
+const moreThanFive = [1, 2, 3, 4, 5, 6, 7]
+console.log(moreThanFive.length)
+let firstNumber = moreThanFive[0]
+console.log(firstNumber)
+let middleNumber = moreThanFive[3]
+console.log(middleNumber)
+let lastNumber = moreThanFive[6]
+console.log(lastNumber)
+const mixedDataTypes = [5, "Twenty one", true, 250, "Four", false, 2]
+console.log(mixedDataTypes.length)
+*/
