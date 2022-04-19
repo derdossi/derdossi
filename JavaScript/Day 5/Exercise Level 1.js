ab845203e36f1c37cd3ec3bb598baa081c198fdc
derdossi <manuelds@hotmail.de>
2022-04-19 22:53:52 +0200

    Day 5

--- JavaScript/Day 5/Exercise Level 1.js
+++ JavaScript/Day 5/Exercise Level 1.js
@@ -11,3 +11,47 @@ console.log(lastNumber)
 const mixedDataTypes = [5, "Twenty one", true, 250, "Four", false, 2]
 console.log(mixedDataTypes.length)
 */
+const itCompanies = [`Facebook`, `Google`, `Microsoft`, `Apple`, `IBM`, `Oracle`, `Amazon`]
+console.log(itCompanies);
+console.log(itCompanies.length);
+let itCompaniesFirst = itCompanies[0]
+console.log(itCompaniesFirst)
+let itCompaniesMiddle = itCompanies[3]
+console.log(itCompaniesMiddle)
+let itCompaniesEnd = itCompanies[6]
+console.log(itCompaniesEnd)
+let itCompaniesSecond = itCompanies[1]
+console.log(itCompaniesSecond)
+let itCompaniesThird = itCompanies[2]
+console.log(itCompaniesThird)
+let itCompaniesFifth = itCompanies[4]
+console.log(itCompaniesFifth)
+let itCompaniesSixth = itCompanies[5]
+console.log(itCompaniesSixth)
+let itCompaniesFirstLower = itCompaniesFirst.toLowerCase()
+let itCompaniesSecondLower = itCompaniesSecond.toLowerCase()
+let itCompaniesThirdLower = itCompaniesThird.toLowerCase()
+let itCompaniesMiddleLower = itCompaniesMiddle.toLowerCase()
+let itCompaniesFifthLower = itCompaniesFifth.toLowerCase()
+let itCompaniesSixthLower = itCompaniesSixth.toLowerCase()
+let itCompaniesEndLower = itCompaniesEnd.toLowerCase()
+console.log(itCompaniesFirstLower)
+console.log(itCompaniesSecondLower)
+console.log(itCompaniesThirdLower)
+console.log(itCompaniesMiddleLower)
+console.log(itCompaniesFifthLower)
+console.log(itCompaniesSixthLower)
+console.log(itCompaniesEndLower)
+console.log(`${itCompanies} are big IT companies. `)
+let index = itCompanies.indexOf("Facebook")
+
+if (index === 0) {
+    console.log("This Company exists in the Array")
+} else {
+    console.log("This Company does not exist in the Array")
+}
+console.log(itCompanies.sort())
+console.log(itCompanies.reverse())
+console.log(itCompanies.slice(0, 3))
+console.log(itCompanies.slice(4, 7))
+console.log(itCompanies.slice(Math.floor(itCompanies.length / 2), 1))
