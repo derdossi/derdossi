80b9c7ca4d9e9c1e7c8eaa37907a546b37197442
TheDossi <manuelds@hotmail.de>
2022-06-23 16:01:07 +0200

    wweiter

--- JavaScript/Frei Uebung/3.175.js
+++ JavaScript/Frei Uebung/3.175.js
@@ -2,7 +2,7 @@ function findDuplicates() {
   const number = [2, 4, 5, 2, 5, 8, 5, 4711];
   for (let i = 0; i < number.length; i++) {
     const numberATI = number[i];
-    for (let j = 0; j < number.length; j++) {
+    for (let j = i + 1; j < number.length; j++) {
       const numberATJ = number[j];
       if (numberATI === numberATJ) {
         console.log(`doppelte Zahl gefunden: ${numberATI}`);
