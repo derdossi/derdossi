612ec2cfd9603bc29f375959ff5653c5fcd84619
TheDossi <manuelds@hotmail.de>
2022-07-10 13:13:57 +0200

    more C

--- C/input and Output.c
+++ C/input and Output.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 int main()
 {
     int x = 3;
-    double y = 3.5;
+    double y = 3.589;
+    float f = 5.6;
+    char c = 'A';
+    _Bool b = true;
+    char name[] = "Ali Badran";
 
-    printf("x=%d\ny=%lf\n", y, x);
+    printf("x=%d\ny=%.1lf\nf=%f\nc=%c\nb=%d\nname=%s\n", x, y, f, c, b, name);
 
     return 0;
 }
