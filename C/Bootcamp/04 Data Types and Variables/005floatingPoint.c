5b14a91743d6623337f9c924bc595187509e8894
derdossi <manuelds@hotmail.de>
2022-08-25 18:42:51 +0200

    typedef

--- C/Bootcamp/04 Data Types and Variables/005floatingPoint.c
+++ C/Bootcamp/04 Data Types and Variables/005floatingPoint.c
@@ -14,7 +14,7 @@ double plankConstant = 6.626E-34;
 
 printf("%12.1f | 1 decimal digit\n", decimalDigits);
 printf("%12.3f | 3 decimal digits\n", decimalDigits);
-printf("%12.1f | WorldPopulation, decimal, 1 digit of precistion\n", worldPopulation);
+printf("%12.1f | WorldPopulation, decimal, 1 digit of precision\n", worldPopulation);
 printf("%12.1e | WorldPopulation, scientific, 1 digit of precision\n", worldPopulation);
 printf("%12.3e | WorldPopulation, scientific, 3 digit of precision\n", worldPopulation);
 printf("%12.1E | Plankconstant, scientific, 1 digit of precision\n", plankConstant);
--- C/Bootcamp/04 Data Types and Variables/008Enum.c
+++ C/Bootcamp/04 Data Types and Variables/008Enum.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-enum menu
+typedef enum
 {
     TEA = 10,    // 10
     COFFEE = 20, // 20
     JUICE = 30,  // 30
     BEER = 40,   // 40
-};
+} Menu;
 
 int main()
 {
-    enum menu mario = COFFEE;
-    enum menu andrea = BEER;
+    Menu mario = COFFEE;
+    Menu andrea = BEER;
 
     printf("Mario ordered: %d\n", mario);
     printf("Andrea ordered: %d\n", andrea);
